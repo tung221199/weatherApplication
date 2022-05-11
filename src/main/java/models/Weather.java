cb8bc0492577d1f6952ab9a4034a6a53625cf448
tttung7 <tungcsp221199@gmail.com>
2022-05-11 16:48:21 +0700

    Test

--- src/main/java/models/Weather.java
+++ src/main/java/models/Weather.java
@@ -85,4 +85,5 @@ public class Weather {
                 ", cityID=" + cityID +
                 '}';
     }
+
 }
--- src/main/java/service/WeatherService.java
+++ src/main/java/service/WeatherService.java
@@ -1,4 +1,8 @@
 package service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class WeatherService {
+
 }
